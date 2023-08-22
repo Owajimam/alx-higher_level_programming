d4889c76ae78d1e577583dedd1f2ce0c50b16911
Owajimam <claire.dienye@aol.co.uk>
2023-08-22 07:01:50 -0700

    Task 0

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -9,12 +9,12 @@
  * @next: Leads to the next node
  *
  * Context: singly linked list node structure
- * 
+ *
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
